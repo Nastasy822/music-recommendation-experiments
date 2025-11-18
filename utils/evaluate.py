6cfa77777fb3afd1d5349b6e347869ae2f0569d4
Anastasia Strueva <a.strueva@itssupport.ru>
2025-11-18 17:25:42 +0500

    feat: update hyprid model

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -33,13 +33,23 @@ def evaluate_model(model, train_df, test_df, k=10):
     for uid, group in tqdm(grouped_users):
         # print(list(group["item_id"]))
         user_true = list(set(group["item_id"]))
-        
+
+        # print("-------------------")
         rec, weights = model.recommend(uid)
-        
         rec, weights = filtering_listened_items(train_df, uid, rec, weights)
-        
-        recall = recall_at_k(rec, user_true,  10)
-        ndcg = ndcg_at_k(rec ,user_true,  10)
+    
+        # print(rec)
+        user_listened_items = set(get_user_listened_items(train_df, uid))
+        perfect = list(set(user_true)-set(user_listened_items))
+
+        if len(perfect) == 0:
+            continue   # skip user
+
+        recall = recall_at_k(rec, perfect,  10)
+        ndcg = ndcg_at_k(rec ,perfect,  10)
+
+        # print(uid, ndcg, len(rec))
+
         
         overall_recall.append(recall)
         overall_ndcg.append(ndcg)
