144e7a50b1192731ad266c152dcb2615289a04c1
Anastasia Strueva <a.strueva@itssupport.ru>
2025-11-30 02:48:31 +0500

    feat: add base stages for dvc

---
+++ stages/data_preprocessing.py
@@ -0,0 +1,15 @@
+import logging
+
+import numpy as np
+import pandas as pd
+
+from stages.base_stage import BaseStage
+
+
+class DataPreprocessing(BaseStage):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def run(self):
+        print("хихик")
